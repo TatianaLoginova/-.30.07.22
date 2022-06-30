2be4eaf2a0426bb6bb1fcdd86a71bc00411a9188
pandora <pandora_tl@yahoo.com>
2022-06-30 11:58:53 +0300

    задача1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int a, b;
+
+Console.Write("Введите певое число: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+    Console.Write("Максимальное число " + a);
+} 
+else
+{
+    Console.Write("Максимальное число " + b);
+}
+
